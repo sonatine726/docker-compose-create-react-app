6704d994c52f84abe0c05582a9c2ff50f189f907
takashik726 <takashik@atmoph.com>
2021-04-26 22:51:32 +0900

    Change the package install timing to run at the first up of container

--- docker_start_react/entrypoint.sh
+++ docker_start_react/entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 set -eu
 
+CHECK_INIT_FILE_PATH='.inited'
+if [ ! -e $CHECK_INIT_FILE_PATH ]; then
+    touch $CHECK_INIT_FILE_PATH
+    echo "Install packages at only Initial run"
+    yarn install
+fi
+
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
